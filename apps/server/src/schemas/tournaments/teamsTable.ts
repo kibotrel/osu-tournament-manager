c9762e030eaa8c13a154a2eb17408079694d9331
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-04-29 01:13:11 +0200

    feat(server/schemas): create tournaments.teams table

---
+++ apps/server/src/schemas/tournaments/teamsTable.ts
@@ -0,0 +1,28 @@
+import type { InferInsertModel, InferSelectModel } from 'drizzle-orm';
+import { integer, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+import { tournamentsSchema } from '#src/schemas/tournaments/tournamentsSchema.js';
+import { tournamentsTable } from '#src/schemas/tournaments/tournamentsTable.js';
+
+export const teamsTable = tournamentsSchema.table('teams', {
+  createdAt: timestamp('createdAt').notNull().defaultNow(),
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 32 }).notNull(),
+  tournamentId: integer('tournamentId')
+    .notNull()
+    .references(
+      () => {
+        return tournamentsTable.id;
+      },
+      { onDelete: 'cascade' },
+    ),
+  updatedAt: timestamp('updatedAt')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => {
+      return new Date();
+    }),
+});
+
+export type SelectTeam = InferSelectModel<typeof teamsTable>;
+export type InsertTeam = InferInsertModel<typeof teamsTable>;
