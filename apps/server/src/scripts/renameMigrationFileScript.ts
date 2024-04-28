ed27fe41ac8099f165c548cfba84a0162e6d9303
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-04-29 01:07:32 +0200

    feat(dev): rename sql migration files script

---
+++ apps/server/src/scripts/renameMigrationFileScript.ts
@@ -0,0 +1,42 @@
+/* eslint-disable unicorn/no-process-exit */
+import { readdir, rename, writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import journal from '../../migrations/meta/_journal.json' assert { type: 'json' };
+
+const fileName = process.argv.at(2);
+const migrationsDirectory = fileURLToPath(
+  new URL('../../migrations', import.meta.url),
+);
+
+if (!fileName) {
+  process.exit(0);
+}
+
+const filenames = await readdir(migrationsDirectory);
+const lastMigrationFilename = filenames.at(-2);
+
+if (!lastMigrationFilename) {
+  process.exit(0);
+}
+
+const [id] = lastMigrationFilename.split('_');
+const targetEntry = journal.entries.find((entry) => {
+  return entry.idx === Number(id);
+});
+
+if (!targetEntry) {
+  process.exit(0);
+}
+
+targetEntry.tag = `${id}_${fileName}`;
+
+await writeFile(
+  join(migrationsDirectory, `meta/_journal.json`),
+  JSON.stringify(journal, undefined, 2),
+);
+await rename(
+  join(migrationsDirectory, lastMigrationFilename),
+  join(migrationsDirectory, `${targetEntry.tag}.sql`),
+);
