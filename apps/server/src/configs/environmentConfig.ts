bc373466c21372c2cb647dd1315a309091096c6c
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-05-01 02:11:05 +0200

    feat(server/configs): add base environment config

---
+++ apps/server/src/configs/environmentConfig.ts
@@ -0,0 +1,5 @@
+export const environmentConfig = {
+  isDevelopmentMode: process.env.NODE_ENV === 'development',
+  isProductionMode: process.env.NODE_ENV === 'production',
+  isTestMode: process.env.NODE_ENV === 'test',
+};
