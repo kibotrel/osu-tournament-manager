aa23a5b5f5fd4b53d28228c936b72436dabc2b4e
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-05-01 02:11:55 +0200

    feat(server): serve api documentation as enpoint through swagger-ui

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,12 @@
-const hello = 'Hello, World!';
+import express from 'express';
+import * as swaggerUi from 'swagger-ui-express';
 
-console.log(hello);
+import { documentation } from '#src/dependencies/documentationDependency.js';
+
+const app = express();
+
+app.use('/documentation', swaggerUi.serve, documentation);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
