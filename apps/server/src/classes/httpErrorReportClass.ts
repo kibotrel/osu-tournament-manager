6bdf2fe11856d33552d10fb8160f5e03372ca9d1
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-05-13 01:30:41 +0200

    feat(server/classes): add RFC 9457 compliant HTTP error report to send to client

---
+++ apps/server/src/classes/httpErrorReportClass.ts
@@ -0,0 +1,55 @@
+import type { Request } from 'express';
+
+import type { HttpError } from '#src/classes/httpErrorClass.js';
+import {
+  AllowedHttpMethodsOnRessource,
+  HttpStatusCodes,
+  HttpStatusCodesToMessagesMapping,
+} from '#src/constants/httpConstants.js';
+
+/**
+ * {@link https://datatracker.ietf.org/doc/rfc9457/ | RFC 9457} compliant error
+ * report sent to the client when `Content-Type` is `application/problem+json`.
+ */
+export class HttpErrorReport {
+  /** Endpoint where the error occurred. */
+  public readonly instance: string;
+  /** HTTP status code. */
+  public readonly status: HttpStatusCodes;
+  /** HTTP status message. */
+  public readonly title: string;
+  /**
+   * Additional validation errors, related to `express-openapi-validator`.
+   * This isn't not included in {@link https://datatracker.ietf.org/doc/rfc9457/ | RFC 9457}.
+   */
+  public readonly errors?: Array<Record<string, string>> | undefined;
+  /** Additional details about the error. */
+  public readonly detail?: string | undefined;
+
+  constructor(request: Request, error: HttpError) {
+    this.instance = request.path;
+    this.status = error.status ?? HttpStatusCodes.BadRequest;
+    this.title = HttpStatusCodesToMessagesMapping[this.status];
+
+    if (
+      ![
+        HttpStatusCodes.InternalServerError,
+        HttpStatusCodes.NotFound,
+        HttpStatusCodes.MethodNotAllowed,
+      ].includes(this.status)
+    ) {
+      this.errors = error.errors;
+      this.detail = error.message;
+    }
+  }
+
+  /**
+   * Returns the allowed HTTP methods on the related resource.
+   * See {@link https://httpwg.org/specs/rfc9110.html#field.allow | RFC 9110 HTTP `Allow` header}.
+   */
+  public getAllowedMethods() {
+    const endpoint = this.instance.replaceAll(/(?<=\/)\d+/g, 'x');
+
+    return AllowedHttpMethodsOnRessource[endpoint];
+  }
+}
