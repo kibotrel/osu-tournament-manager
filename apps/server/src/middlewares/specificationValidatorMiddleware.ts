9a6d5a4e486ce951d5376bd0302de211c89fd79b
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-05-13 01:38:27 +0200

    feat(server/middlewares): add api specification validator

---
+++ apps/server/src/middlewares/specificationValidatorMiddleware.ts
@@ -0,0 +1,10 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import OpenApiValidator from 'express-openapi-validator';
+
+const directoryName = path.dirname(fileURLToPath(import.meta.url));
+
+export const specificationValidator = OpenApiValidator.middleware({
+  apiSpec: path.join(directoryName, '../assets', 'internal.json'),
+});
