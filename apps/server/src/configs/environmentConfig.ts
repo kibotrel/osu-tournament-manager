503e82d7dd85ee15ac24961294bdcd15c5f4a2b1
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-02 18:30:05 +0200

    fix(server): expressPort environment variable

--- apps/server/src/configs/environmentConfig.ts
+++ apps/server/src/configs/environmentConfig.ts
@@ -1,4 +1,5 @@
 export const environmentConfig = {
+  expressPort: Number(process.env.EXPRESS_PORT) || Number.NaN,
   isDevelopmentMode: process.env.NODE_ENV === 'development',
   isProductionMode: process.env.NODE_ENV === 'production',
   isTestMode: process.env.NODE_ENV === 'test',
