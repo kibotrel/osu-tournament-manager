b939c2c7e7ddaf07b8709acd627deec73b986b4b
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-02 18:28:49 +0200

    fix(client): pinia store router injection

--- apps/client/src/plugins/index.ts
+++ apps/client/src/plugins/index.ts
@@ -1,8 +1,16 @@
+import 'pinia';
 import type { PiniaPluginContext } from 'pinia';
 import type { App } from 'vue';
 import { markRaw } from 'vue';
+import type { Router } from 'vue-router';
 
 import router from '#src/router/index.js';
+
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    $router: Router;
+  }
+}
 
 export const vuePluginRouter = {
   install: (app: App) => {
