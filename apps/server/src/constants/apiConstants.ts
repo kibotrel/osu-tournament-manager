8c0443c4ed5d425bfbd75ce90ab65ac9e846aefd
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-09 01:45:56 +0200

    fix(server/constants): keys in AllowedHttpMethodsOnRessource object to include /api prefix

--- apps/server/src/constants/apiConstants.ts
+++ apps/server/src/constants/apiConstants.ts
@@ -1,4 +1,4 @@
 export const AllowedHttpMethodsOnRessource: Record<string, string> = {
-  '/v1/public/health': 'GET',
-  '/v1/public/oauth': 'POST',
+  '/api/v1/public/health': 'GET',
+  '/api/v1/public/oauth': 'POST',
 };
