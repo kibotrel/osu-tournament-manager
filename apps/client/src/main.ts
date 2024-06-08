f51ee72bc2c571b578da9c41f1c85721f56a711e
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-09 01:27:32 +0200

    feat(client): use geist sans font

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -2,6 +2,16 @@ import { createPinia } from 'pinia';
 import { createPersistedState } from 'pinia-plugin-persistedstate';
 import { createApp } from 'vue';
 
+import '@fontsource/geist-sans/100.css';
+import '@fontsource/geist-sans/200.css';
+import '@fontsource/geist-sans/300.css';
+import '@fontsource/geist-sans/400.css';
+import '@fontsource/geist-sans/500.css';
+import '@fontsource/geist-sans/600.css';
+import '@fontsource/geist-sans/700.css';
+import '@fontsource/geist-sans/800.css';
+import '@fontsource/geist-sans/900.css';
+
 import App from './app.vue';
 import './assets/styles/index.css';
 import { piniaPluginRouter, vuePluginRouter } from './plugins/index.js';
