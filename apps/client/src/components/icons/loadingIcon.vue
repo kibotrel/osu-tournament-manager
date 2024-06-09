c13eacdec9c9951683e6fc85885ebe0ad6f0acd3
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-09 04:56:26 +0200

    feat(client/components): add some icon components

---
+++ apps/client/src/components/icons/loadingIcon.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg viewBox="0 0 40 40" height="40" width="40" overflow="visible">
+    <circle
+      :stroke-width="properties.strokeWidth"
+      cx="20"
+      cy="20"
+      fill="none"
+      opacity="0.25"
+      pathlength="100"
+      r="17.5"
+      stroke="currentColor"
+    />
+    <circle
+      :stroke-width="properties.strokeWidth"
+      cx="20"
+      cy="20"
+      fill="none"
+      pathlength="100"
+      r="17.5"
+      stroke="currentColor"
+      stroke-dasharray="25, 75"
+      stroke-dashoffset="0"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+
+interface Properties {
+  strokeWidth?: string;
+}
+
+const properties = withDefaults(defineProps<Properties>(), {
+  strokeWidth: '3px',
+});
+</script>
+
+<style scoped>
+svg {
+  animation: rotate 0.8s linear infinite;
+}
+
+circle {
+  transition: stroke 0.5s ease;
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
---
+++ apps/client/src/components/ui/uiIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <component :is="svgComponent()" />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent, defineProps } from 'vue';
+
+export type IconNames = 'identification' | 'loading' | 'transparentCube';
+
+const properties = defineProps<{
+  name: IconNames;
+}>();
+
+const svgComponent = () => {
+  return defineAsyncComponent(() => {
+    return import(`#src/components/icons/${properties.name}Icon.vue`);
+  });
+};
+</script>
