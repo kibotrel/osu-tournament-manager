3cba94ca48a3d67f24696d7e98b05eb9e72bfda9
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-06-09 05:04:04 +0200

    feat(client/pages): add temporary redirect page after login

---
+++ apps/client/src/pages/dashboardPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m-4">
+    <p>Welcome {{ username }} ({{ gameUserId }}). Login: {{ isLoggedIn }}</p>
+    <ui-button @mousedown="logout">Logout</ui-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import uiButton from '#src/components/ui/uiButton.vue';
+import { useUserStore } from '#src/stores/userStore.js';
+
+const { gameUserId, username, isLoggedIn, logout } = useUserStore();
+</script>
+
+<style scoped></style>
