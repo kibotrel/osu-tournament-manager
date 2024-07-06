1a2b3bddafc6e70f1466f8fbb51c8c14381bfca3
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-07-06 11:36:16 +0200

    refactor(server/queries): remove return type for createUser query

--- apps/server/src/queries/database/users/createUserQueries.ts
+++ apps/server/src/queries/database/users/createUserQueries.ts
@@ -1,8 +1,8 @@
 import { database } from '#src/dependencies/databaseDependency.js';
-import type { InsertUser, SelectUser } from '#src/schemas/users/usersTable.js';
+import type { InsertUser } from '#src/schemas/users/usersTable.js';
 import { usersTable } from '#src/schemas/users/usersTable.js';
 
-export const createUser = async (userData: InsertUser): Promise<SelectUser> => {
+export const createUser = async (userData: InsertUser) => {
   const [user] = await database.insert(usersTable).values(userData).returning();
 
   return user;
