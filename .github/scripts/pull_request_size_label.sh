a282b635ddb3e50d12abf930fcb4b4c9357dc0b2
kibotrel <45034541+kibotrel@users.noreply.github.com>
2024-09-06 18:46:14 +0200

    ci: add automatic pull request size labeling (#41)

---
+++ .github/scripts/pull_request_size_label.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <PR_NUMBER>"
+  exit 1
+fi
+
+pullRequestNumber=$1
+rawData=$(gh pr view $pullRequestNumber --json additions,deletions,labels --template '{{.additions }} {{.deletions }} {{.labels}}')
+
+read -a rawData <<< $rawData
+
+additions=${rawData[0]}
+deletions=${rawData[1]}
+labels=${rawData[@]:2}
+current_size_label=$(echo $labels | grep -o 'Size: [^] ]*')
+pullRequestSize=$(((additions + deletions) / 2))
+if [ $pullRequestSize -lt 25 ]; then
+  expected_label='Size: Small'
+elif [ $pullRequestSize -lt 100 ]; then
+  expected_label='Size: Medium'
+else
+  expected_label='Size: Large'
+fi
+
+if [ "$current_size_label" != "$expected_label" ]; then
+  gh pr edit $pullRequestNumber --remove-label 'Size: Small','Size: Medium','Size: Large' --add-label "$expected_label" > /dev/null
+fi
