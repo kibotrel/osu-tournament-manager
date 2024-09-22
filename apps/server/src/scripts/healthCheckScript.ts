6c347b24cf0e81611f8ef31d0fc77cb99023614c
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-09-22 16:37:30 +0200

    refactor(server/scripts): exitCode configuration in healthcheck

--- apps/server/src/scripts/healthCheckScript.ts
+++ apps/server/src/scripts/healthCheckScript.ts
@@ -4,6 +4,5 @@ import { environmentConfig } from '#src/configs/environmentConfig.js';
 const response = await fetch(
   `http://localhost:${environmentConfig.expressPort}/api/v1/public/health`,
 );
-const exitCode = response.ok ? 0 : 1;
 
-process.exit(exitCode);
+process.exitCode = response.ok ? 0 : 1;
