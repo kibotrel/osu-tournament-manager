b3901408b7c2e575633dd3d1be6f07e729801572
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-10-06 17:03:19 +0200

    chore(packages): rename export files

--- packages/osu-sdk/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './constants/index.js';
-export * from './oauth/osuPostOauthTokenRequest.js';
-export * from './me/osuGetMe.js';
---
+++ packages/osu-sdk/src/osuSdkExport.ts
@@ -0,0 +1,4 @@
+export * from './api/oauth/token/osuPostOauthTokenRequest.js';
+export * from './api/v2/me/osuGetMe.js';
+export * from './constants/osuConstants.js';
+export * from './types/osuTypes.js';
