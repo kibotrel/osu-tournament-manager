675c9de08c2a6c8bb7fbc0c97916646b41f06ffe
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-10-11 01:22:32 +0200

    fix(apps/server/middlewares): use HttpErrorReport from package shared

--- apps/server/src/middlewares/ressourceNotFoundMiddleware.ts
+++ apps/server/src/middlewares/ressourceNotFoundMiddleware.ts
@@ -1,17 +1,22 @@
 import {
   HttpContentType,
+  HttpErrorReport,
   HttpHeader,
   HttpNotFoundError,
 } from '@packages/shared';
 import type { RequestHandler } from 'express';
 
-import { HttpErrorReport } from '#src/classes/httpErrorReportClass.js';
+import { AllowedHttpMethodsOnRessource } from '#src/constants/serverConstants.js';
 
 export const ressourceNotFoundHandler: RequestHandler = (request, response) => {
   const error = new HttpNotFoundError({
     message: `The ressource at ${request.path} could not be found.`,
   });
-  const errorReport = new HttpErrorReport(request, error);
+  const errorReport = new HttpErrorReport(
+    request,
+    AllowedHttpMethodsOnRessource,
+    error,
+  );
 
   response.setHeader(
     HttpHeader.ContentType,
