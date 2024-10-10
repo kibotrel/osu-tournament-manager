2845d59e71d340774c28fc1eb5b5dee933fc5125
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-10-11 01:24:35 +0200

    fix(apps/server/configs): add nodeEnv and isStagingMode to environmentConfig

--- apps/server/src/configs/environmentConfig.ts
+++ apps/server/src/configs/environmentConfig.ts
@@ -4,6 +4,8 @@ export const environmentConfig = {
   isDevelopmentMode: process.env.NODE_ENV === 'development',
   isProductionMode: process.env.NODE_ENV === 'production',
   isTestMode: process.env.NODE_ENV === 'test',
+  nodeEnv: process.env.NODE_ENV || '',
+  isStagingMode: process.env.NODE_ENV === 'staging',
   osuClientId: Number(process.env.OSU_CLIENT_ID) || Number.NaN,
   osuClientSecret: process.env.OSU_CLIENT_SECRET || '',
   sessionSecret: process.env.SESSION_SECRET || '',
