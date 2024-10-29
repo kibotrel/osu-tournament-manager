61b7e5f51db5cdd83895257624192f8d8a74ae29
BOTREL Kilian <botrel.kilian1999@gmail.com>
2024-10-29 23:02:08 +0100

    chore(apps/server): update import of logger to the right syntax in cacheDepency.ts

--- apps/server/src/dependencies/cacheDependency.ts
+++ apps/server/src/dependencies/cacheDependency.ts
@@ -2,8 +2,7 @@ import type { RedisClientType } from 'redis';
 import { createClient } from 'redis';
 
 import { cacheConfig } from '#src/configs/cacheConfig.js';
-
-import { logger } from './loggerDependency.js';
+import { logger } from '#src/dependencies/loggerDependency.js';
 
 const cache: RedisClientType = createClient({
   url: `redis://default:${cacheConfig.password}@${cacheConfig.host}:${cacheConfig.port}`,
