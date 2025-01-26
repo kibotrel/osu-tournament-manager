8e84368fa76fb874280a78674d54f3c9fcb7792d
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-01-26 20:19:53 +0100

    feat(packages/shared/constants): add WebSocketClosureCode and WebSocketClosureReason enums

--- packages/shared/src/constants/webSocketConstants.ts
+++ packages/shared/src/constants/webSocketConstants.ts
@@ -20,3 +20,29 @@ export const WEBSOCKET_PONG_TIMEOUT = 45 * Time.Second;
  * [RFC 6455](https://tools.ietf.org/html/rfc6455#section-5.5.2) compliant pong payload.
  */
 export const WEBSOCKET_PONG_PAYLOAD = 10;
+
+/**
+ * [RFC 6455](https://tools.ietf.org/html/rfc6455#section-7.4.1) compliant connection closure codes.
+ * Some codes are deliberately omitted as they are not relevant to the current implementation.
+ */
+export enum WebSocketClosureCode {
+  /** The endpoint is going away, either because of a server failure or because the browser is navigating away from the page that opened the connection. */
+  GoingAway = 1001,
+  /** Indicates that an endpoint is terminating the connection because it has received a message that is too big. */
+  MessageTooLarge = 1009,
+  /** Normal closure, the connection successfully completed whatever purpose for which it was created. */
+  Normal = 1000,
+  /**
+   * Indicates an arbitrary set policy violation. Can be used to hide details for security reasons.
+   * It's the equivalent of an Internal Server Error in HTTP.
+   */
+  PolicyViolation = 1008,
+  /** Used when receiving unexpected data (e.g text when binary was expected or vice versa). */
+  UnsupportedData = 1003,
+}
+
+export enum WebSocketClosureReason {
+  NotResponding = 'Not responding',
+  Reconnecting = 'Reconnecting',
+  ServerShutdown = 'Server shutdown',
+}
