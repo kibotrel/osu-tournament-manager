1245e99974a82906c9d26c6f23ad2ff4c9fc4121
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-02-10 23:40:36 +0100

    feat(packages/shared/methods): add formatTimestamp

---
+++ packages/shared/src/methods/numberMethods.ts
@@ -0,0 +1,13 @@
+/**
+ * Format UTC timestamp to a human-readable string in `HH:mm:ss` format.
+ */
+export const formatTimestamp = (timestamp: number): string => {
+  return new Intl.DateTimeFormat(undefined, {
+    hour: '2-digit',
+    hour12: false,
+    minute: '2-digit',
+    second: '2-digit',
+    // TODO: Add Timezone support based on https://timezonedb.com/time-zones
+    timeZone: 'UTC',
+  }).format(timestamp);
+};
