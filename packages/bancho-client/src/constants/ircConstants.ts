eb0e94035782efcb37dbee070ba9bb4e4245ed25
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-02-25 22:47:39 +0100

    feat(packages/bancho-client): add IrcKeyword enum

--- packages/bancho-client/src/constants/ircConstants.ts
+++ packages/bancho-client/src/constants/ircConstants.ts
@@ -11,3 +11,24 @@ export enum IrcClientState {
   Connecting = 'connecting',
   Connected = 'connected',
 }
+
+/**
+ * List of [RFC 1459](https://tools.ietf.org/html/rfc1459) compliant IRC commands.
+ */
+export enum IrcKeyword {
+  ChannelTopic = '332',
+  ChannelTopicUpdatedAt = '333',
+  EndOfNameList = '366',
+  MessageOfTheDayBegin = '375',
+  MessageOfTheDayBody = '372',
+  MessageOfTheDayEnd = '376',
+  Mode = 'MODE',
+  NameListBody = '353',
+  Nickname = 'NICK',
+  Password = 'PASS',
+  Ping = 'PING',
+  Pong = 'PONG',
+  Quit = 'QUIT',
+  Username = 'USER',
+  Welcome = '001',
+}
