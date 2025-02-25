c64aa147d936f81da81eaa34d72519c336a786ea
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-02-26 00:06:54 +0100

    feat(apps/server): load bancho credentials

--- apps/server/src/configs/environmentConfig.ts
+++ apps/server/src/configs/environmentConfig.ts
@@ -8,5 +8,9 @@ export const environmentConfig = {
   isStagingMode: process.env.NODE_ENV === 'staging',
   osuClientId: Number(process.env.OSU_CLIENT_ID) || Number.NaN,
   osuClientSecret: process.env.OSU_CLIENT_SECRET || '',
+  osuIrcClientPassword: process.env.OSU_IRC_CLIENT_PASSWORD || '',
+  osuIrcClientUsername: process.env.OSU_IRC_CLIENT_USERNAME || '',
+  osuIrcServerHostname: 'cho.ppy.sh',
+  osuIrcServerPort: 6667,
   sessionSecret: process.env.SESSION_SECRET || '',
 };
