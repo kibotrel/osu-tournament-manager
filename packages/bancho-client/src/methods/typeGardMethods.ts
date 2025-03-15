e424c7b5d7712b95e9454431fca36b22f9ab802d
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-03-15 17:12:16 +0100

    feat(packages/bancho-client): add guards to detect if recipient is public channel or direct message

--- packages/bancho-client/src/methods/typeGardMethods.ts
+++ packages/bancho-client/src/methods/typeGardMethods.ts
@@ -3,6 +3,14 @@ import type { Socket } from 'node:net';
 import type { IrcClientState } from '#src/constants/ircConstants.js';
 import { clientStatesAllowingMessage } from '#src/constants/ircConstants.js';
 
+export const isDirectMessageChannel = (recipient: string) => {
+  return !isPublicChannel(recipient);
+};
+
+export const isPublicChannel = (recipient: string) => {
+  return recipient.startsWith('#');
+};
+
 export const isSocketReady = (
   socket: unknown,
   state: IrcClientState,
