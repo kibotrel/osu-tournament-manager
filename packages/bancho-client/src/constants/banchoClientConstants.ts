538ae518cbb1f80310dc7c530b495accb6788539
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-03-25 17:32:50 +0100

    feat(packages/bancho-client): add BanchoCommand and BanchoPublicChannel enums

--- packages/bancho-client/src/constants/banchoClientConstants.ts
+++ packages/bancho-client/src/constants/banchoClientConstants.ts
@@ -12,3 +12,33 @@ export enum BanchoClientEvent {
   UserJoinedChannel = 'user_joined_channel',
   UserLeftChannel = 'user_left_channel',
 }
+
+/**
+ * Commands that can be used to manage a tournament match.
+ * See [the osu! Tournament management commands wiki](https://osu.ppy.sh/wiki/en/osu%21_tournament_client/osu%21tourney/Tournament_management_commands)
+ * for more information.
+ */
+export enum BanchoCommand {
+  AbortMatch = '!mp abort',
+  AbortTimer = '!mp aborttimer',
+  ClearHost = '!mp clearhost',
+  CloseMatch = '!mp close',
+  CreateTournamentMatch = '!mp make',
+  GetMatchSettings = '!mp settings',
+  InvitePlayer = '!mp invite',
+  KickPlayer = '!mp kick',
+  LockMatchState = '!mp lock',
+  MovePlayer = '!mp move',
+  SetBeatmap = '!mp map',
+  SetLobbySize = '!mp size',
+  SetMatchProperty = '!mp set',
+  SetMods = '!mp mods',
+  SetPlayerTeam = '!mp team',
+  StartMatch = '!mp start',
+  StartTimer = '!mp timer',
+  TransferHost = '!mp host',
+}
+
+export enum BanchoPublicChannel {
+  Lobby = '#lobby',
+}
