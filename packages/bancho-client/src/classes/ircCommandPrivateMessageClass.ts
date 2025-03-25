b0fa1411c030bee24de9e80bb2fdafd92807b7f4
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-03-25 17:41:26 +0100

    feat(packages/bancho-client): emit MultiplayerChannelClosed event when BanchoBot tells it

--- packages/bancho-client/src/classes/ircCommandPrivateMessageClass.ts
+++ packages/bancho-client/src/classes/ircCommandPrivateMessageClass.ts
@@ -1,6 +1,10 @@
 import { BanchoClientEvent } from '#src/banchoClientExport.js';
 import type { BanchoClient } from '#src/classes/ircClientClass.js';
 import type { IrcCommand } from '#src/classes/ircCommandClass.js';
+import {
+  BanchoCommonMessage,
+  BanchoUser,
+} from '#src/constants/banchoClientConstants.js';
 import { parseIrcUsername } from '#src/methods/parseMethods.js';
 
 export class IrcCommandPrivateMessage implements IrcCommand {
@@ -16,12 +20,27 @@ export class IrcCommandPrivateMessage implements IrcCommand {
   public handleCommand() {
     const [user, , channel] = this.packetParts.at(0)!.split(' ');
     const message = this.packetParts.slice(1).join(':');
+    const sanitizedUser = parseIrcUsername(user.split('!').at(0)!);
 
     this.banchoClient.emit(
       BanchoClientEvent.ChannelMessage,
-      parseIrcUsername(user.split('!').at(0)!),
+      sanitizedUser,
       channel,
       message,
     );
+
+    // TODO: Probably going to refactor this as a separate function when adding more cases
+    if (
+      sanitizedUser === BanchoUser.BanchoBot &&
+      message === BanchoCommonMessage.ClosedMatch
+    ) {
+      this.banchoClient.emit(
+        BanchoClientEvent.MultiplayerChannelClosed,
+        channel,
+      );
+      this.banchoClient.emit(
+        `${BanchoClientEvent.MultiplayerChannelClosed}:${channel}`,
+      );
+    }
   }
 }
