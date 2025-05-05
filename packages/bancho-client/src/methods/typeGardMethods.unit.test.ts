2bb1dcfdab03e2095cccbcd6fcc09434727a8ee1
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-05-06 00:55:29 +0200

    test(packages/bancho-client): add isSocketReady unit tests

--- packages/bancho-client/src/methods/typeGardMethods.unit.test.ts
+++ packages/bancho-client/src/methods/typeGardMethods.unit.test.ts
@@ -1,8 +1,12 @@
+import { Socket } from 'node:net';
+
 import { describe, expect, it } from 'vitest';
 
+import { IrcClientState } from '#src/constants/ircConstants.js';
 import {
   isDirectMessageChannel,
   isPublicChannel,
+  isSocketReady,
 } from '#src/methods/typeGardMethods.js';
 
 describe('isDirectMessageChannel', () => {
@@ -22,5 +26,33 @@ describe('isPublicChannel', () => {
 
   it('should return false if input is a direct message channel', () => {
     expect(isPublicChannel('username')).toBe(false);
+  });
+});
+
+describe('isSocketReady', () => {
+  it('should return true if socket is writtable and given state is connected', () => {
+    expect(isSocketReady(IrcClientState.Connected, new Socket())).toBe(true);
+  });
+
+  it('should return true if socket is writtable and given state is connecting', () => {
+    expect(isSocketReady(IrcClientState.Connecting, new Socket())).toBe(true);
+  });
+
+  it('should return false if socket is not writable', () => {
+    expect(
+      isSocketReady(IrcClientState.Connected, new Socket().destroy()),
+    ).toBe(false);
+  });
+
+  it('should return false if socket is writable and given state is disconnected', () => {
+    expect(isSocketReady(IrcClientState.Disconnected, new Socket())).toBe(
+      false,
+    );
+  });
+
+  it('should return false if socket is writable and given state is disconnecting', () => {
+    expect(isSocketReady(IrcClientState.Disconnecting, new Socket())).toBe(
+      false,
+    );
   });
 });
