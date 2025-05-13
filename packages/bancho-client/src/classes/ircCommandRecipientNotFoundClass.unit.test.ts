560358aa89e54f7d5b2ada8e4017b0188390cf55
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-05-13 18:45:14 +0200

    ci(packages/bancho-client): add IrcCommandRecipientNotFound unit tests

---
+++ packages/bancho-client/src/classes/ircCommandRecipientNotFoundClass.unit.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it, vi } from 'vitest';
+
+import { BanchoClient } from '#src/classes/ircClientClass.js';
+
+import { IrcCommandRecipientNotFound } from './ircCommandRecipientNotFoundClass.js';
+
+describe('IrcCommandRecipientNotFound', () => {
+  const banchoClient = new BanchoClient({
+    clientCredentials: { username: 'username', password: 'password' },
+    serverInformation: { host: 'localhost.dev', port: 6667 },
+  });
+  const packetParts = ['localhost.dev 401 username1 username2', 'No such nick'];
+
+  describe('constructor', () => {
+    it('should create an instance of IrcCommandRecipientNotFound', () => {
+      const command = new IrcCommandRecipientNotFound(
+        banchoClient,
+        packetParts,
+      );
+
+      expect(command).toBeInstanceOf(IrcCommandRecipientNotFound);
+      expect(command).toHaveProperty('banchoClient', banchoClient);
+      expect(command).toHaveProperty('packetParts', packetParts);
+    });
+  });
+
+  describe('handleCommand', () => {
+    it('should emit recipient_not_found event with the corresponding recipient', () => {
+      const command = new IrcCommandRecipientNotFound(
+        banchoClient,
+        packetParts,
+      );
+      const eventEmitter = vi.spyOn(banchoClient, 'emit');
+
+      command.handleCommand();
+
+      expect(eventEmitter).toHaveBeenCalledTimes(2);
+      expect(eventEmitter).toHaveBeenCalledWith('recipient_not_found', {
+        recipient: 'username2',
+      });
+      expect(eventEmitter).toHaveBeenCalledWith(
+        'recipient_not_found:username2',
+      );
+    });
+  });
+});
