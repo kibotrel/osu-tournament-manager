e02939315c5fc0995918865959bec97a19912f76
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-07-23 01:15:23 +0200

    ci(apps/server): add next, request and responses mock utilities

---
+++ apps/server/src/tests/expressMocks.ts
@@ -0,0 +1,77 @@
+import { randomUUID } from 'node:crypto';
+
+import { Time } from '@packages/shared';
+import type { Request, Response } from 'express';
+import { vi } from 'vitest';
+
+export const expressNextFunctionMock = () => {
+  return vi.fn();
+};
+
+export const expressRequestMock = <
+  PathParametersType = never,
+  ResponseBodyType = never,
+  RequestBodyType = never,
+  RequestQueryType = never,
+>() => {
+  const request: Partial<
+    Request<
+      PathParametersType,
+      ResponseBodyType,
+      RequestBodyType,
+      RequestQueryType
+    >
+  > = {};
+
+  request.session = {
+    cookie: {
+      path: '/',
+      secure: true,
+      httpOnly: true,
+      originalMaxAge: Time.Week,
+    },
+    destroy: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+    id: randomUUID(),
+    reload: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+    regenerate: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+    resetMaxAge: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+    save: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+    touch: vi.fn().mockImplementation(() => {
+      return request.session;
+    }),
+  };
+
+  return request as Request<
+    PathParametersType,
+    ResponseBodyType,
+    RequestBodyType,
+    RequestQueryType
+  >;
+};
+
+export const expressResponseMock = <ResponseBodyType = never>() => {
+  const response: Partial<Response<ResponseBodyType>> = {};
+
+  response.json = vi.fn().mockImplementation(() => {
+    return response;
+  });
+
+  response.end = vi.fn().mockImplementation(() => {
+    return response;
+  });
+  response.status = vi.fn().mockImplementation(() => {
+    return response;
+  });
+
+  return response as Response<ResponseBodyType>;
+};
