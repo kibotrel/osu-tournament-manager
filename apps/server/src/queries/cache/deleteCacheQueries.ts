c9f1cd1052f56f0ce8fd577268fd228b7c06c50f
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-07-28 23:38:07 +0200

    fix(apps/server): await lRange call in popCacheArrayByKey

--- apps/server/src/queries/cache/deleteCacheQueries.ts
+++ apps/server/src/queries/cache/deleteCacheQueries.ts
@@ -2,7 +2,7 @@ import type { CacheKey } from '#src/@types/cache/cacheTypes.js';
 import { cache } from '#src/dependencies/cacheDependency.js';
 
 export const popCacheArrayByKey = async (key: CacheKey) => {
-  const array = cache.lRange(key, 0, -1);
+  const array = await cache.lRange(key, 0, -1);
 
   await cache.del(key);
 
