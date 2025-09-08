169a2d19f0b459acf1b5b8ede14531fc6b40124c
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-09-08 03:40:56 +0200

    chore(apps/client): sort api calls alphabetically

--- apps/client/src/api/publicApi.ts
+++ apps/client/src/api/publicApi.ts
@@ -13,6 +13,23 @@ import type { Router } from 'vue-router';
 import { baseUrl } from '#src/api/apiConstants.js';
 import { useUserStore } from '#src/stores/userStore.js';
 
+const postPublicLogin = async (authenticationCode: string) => {
+  const response = await postRequest<
+    PostPublicLoginRequestBody,
+    PostPublicLoginResponseBody
+  >({
+    baseUrl,
+    endpoint: '/public/login',
+    payload: { authenticationCode },
+  });
+
+  if (!response.isOk) {
+    throw new Error(JSON.stringify(response.data));
+  }
+
+  return response.data as PostPublicLoginResponseData;
+};
+
 const getPublicLogout = async () => {
   const response = await getRequest<
     GetPublicLogoutRequestBody,
@@ -28,45 +45,6 @@ const getPublicLogout = async () => {
   }
 
   return response.data;
-};
-
-/**
- * Log out the user and destroy its session.
- */
-export const usePostPublicLogout = () => {
-  const router = inject<Router>('$router');
-
-  return useMutation<GetPublicLogoutResponseBody, Error>({
-    mutationFn: async () => {
-      return await getPublicLogout();
-    },
-    onSuccess: async () => {
-      const { resetUser } = useUserStore();
-
-      resetUser();
-      await router?.push('/login');
-    },
-  });
-};
-
-/**
- * Exchange an authentication code from osu! Oauth for a user bearer token + init a session.
- */
-export const postPublicLogin = async (authenticationCode: string) => {
-  const response = await postRequest<
-    PostPublicLoginRequestBody,
-    PostPublicLoginResponseBody
-  >({
-    baseUrl,
-    endpoint: '/public/login',
-    payload: { authenticationCode },
-  });
-
-  if (!response.isOk) {
-    throw new Error(JSON.stringify(response.data));
-  }
-
-  return response.data as PostPublicLoginResponseData;
 };
 
 /**
@@ -86,6 +64,25 @@ export const usePostPublicLogin = () => {
       await router?.push('/');
     },
     onError: async () => {
+      await router?.push('/login');
+    },
+  });
+};
+
+/**
+ * Log out the user and destroy its session.
+ */
+export const usePostPublicLogout = () => {
+  const router = inject<Router>('$router');
+
+  return useMutation<GetPublicLogoutResponseBody, Error>({
+    mutationFn: async () => {
+      return await getPublicLogout();
+    },
+    onSuccess: async () => {
+      const { resetUser } = useUserStore();
+
+      resetUser();
       await router?.push('/login');
     },
   });
