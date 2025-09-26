65788d8272ce3bf7de9555f43217be27a5158d05
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-09-26 02:23:03 +0200

    feat(apps/clients/components): add ArrowRightEndOnRectangleIcon

--- apps/client/src/components/base/baseIcon.vue
+++ apps/client/src/components/base/baseIcon.vue
@@ -8,6 +8,7 @@ import { defineAsyncComponent } from 'vue';
 export type IconNames =
   | 'archiveBox'
   | 'arrowLeftStartOnRectangle'
+  | 'arrowRightEndOnRectangle'
   | 'cubeTransparent'
   | 'identification'
   | 'loading'
