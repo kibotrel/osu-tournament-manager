7e2b3d31588563c140d8b07654ebf3cd61c8f6dc
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-10-07 02:43:57 +0200

    feat(apps/server/dependencies): delete match in cache on  BanchoClientEvent.MultiplayerChannelClosed

--- apps/server/src/dependencies/ircClientDependency.ts
+++ apps/server/src/dependencies/ircClientDependency.ts
@@ -2,6 +2,7 @@ import { BanchoClient, BanchoClientEvent } from '@packages/bancho-client';
 
 import { environmentConfig } from '#src/configs/environmentConfig.js';
 import { logger } from '#src/dependencies/loggerDependency.js';
+import { removeMatchFromCachedSet } from '#src/services/cache/cacheService.js';
 
 const banchoClient = new BanchoClient({
   clientCredentials: {
@@ -18,12 +19,23 @@ banchoClient.on(BanchoClientEvent.BotConnected, () => {
   logger.debug('[IRC] Connected to the osu! server.');
 });
 
+banchoClient.on(BanchoClientEvent.BotDisconnected, async () => {
+  logger.debug('[IRC] Disconnected from osu! server');
+
+  await banchoClient.connect();
+});
+
 banchoClient.on(BanchoClientEvent.BotJoinedChannel, ({ channel }) => {
   logger.debug(`[IRC] Joined ${channel}.`);
 });
 
-banchoClient.on(BanchoClientEvent.MultiplayerChannelClosed, ({ channel }) => {
-  logger.debug(`[IRC] Match ${channel} closed.`);
-});
+banchoClient.on(
+  BanchoClientEvent.MultiplayerChannelClosed,
+  async ({ channel }) => {
+    logger.debug(`[IRC] Match ${channel} closed.`);
+
+    await removeMatchFromCachedSet(channel);
+  },
+);
 
 export { banchoClient };
