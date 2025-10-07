e7feffc4498967b2e7fa8605a4c5d64d04bde239
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-10-07 02:45:39 +0200

    feat(apps/serve/queries): add expiry support to cache operations

--- apps/server/src/queries/cache/updateCacheQueries.ts
+++ apps/server/src/queries/cache/updateCacheQueries.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-ternary */
 import type {
   AddToCacheOptions,
   CacheListKey,
@@ -8,31 +9,47 @@ import { cache } from '#src/dependencies/cacheDependency.js';
 export const addToListInCacheByKey = async (
   options: AddToCacheOptions<CacheListKey>,
 ) => {
-  const { key, value } = options;
+  const { key, value, expiryInSeconds } = options;
 
-  await cache.rPush(key, value);
+  if (expiryInSeconds) {
+    await cache.multi().rPush(key, value).expire(key, expiryInSeconds).exec();
+  } else {
+    await cache.rPush(key, value);
+  }
 };
 
 export const addToSetInCacheByKey = async (
   options: AddToCacheOptions<CacheSetKey>,
 ) => {
-  const { key, value } = options;
+  const { expiryInSeconds, key, value } = options;
 
-  await cache.sAdd(key, value);
+  if (expiryInSeconds) {
+    await cache.multi().sAdd(key, value).expire(key, expiryInSeconds).exec();
+  } else {
+    await cache.sAdd(key, value);
+  }
 };
 
 export const removeFromListInCacheByKey = async (
   options: AddToCacheOptions<CacheListKey>,
 ) => {
-  const { key, value } = options;
+  const { expiryInSeconds, key, value } = options;
 
-  await cache.lRem(key, 0, value);
+  if (expiryInSeconds) {
+    await cache.multi().lRem(key, 0, value).expire(key, expiryInSeconds).exec();
+  } else {
+    await cache.lRem(key, 0, value);
+  }
 };
 
 export const removeFromSetInCacheByKey = async (
   options: AddToCacheOptions<CacheSetKey>,
 ) => {
-  const { key, value } = options;
+  const { expiryInSeconds, key, value } = options;
 
-  await cache.sRem(key, value);
+  if (expiryInSeconds) {
+    await cache.multi().sRem(key, value).expire(key, expiryInSeconds).exec();
+  } else {
+    await cache.sRem(key, value);
+  }
 };
