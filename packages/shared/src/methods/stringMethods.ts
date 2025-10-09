887929402073308c334137eb5e2d74821f7160e0
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-10-09 02:31:24 +0200

    refactor(packages/shared/methods): add safeties on gameMatchIdFromBanchoChannel

--- packages/shared/src/methods/stringMethods.ts
+++ packages/shared/src/methods/stringMethods.ts
@@ -2,5 +2,15 @@
  * Extract the match ID from a Bancho multiplayer channel.
  */
 export const gameMatchIdFromBanchoChannel = (channel: string): number => {
-  return Number(channel.slice(4));
+  if (channel.length <= 4) {
+    return Number.NaN;
+  }
+
+  const id = Number(channel.slice(4));
+
+  if (!Number.isFinite(id)) {
+    return Number.NaN;
+  }
+
+  return id;
 };
