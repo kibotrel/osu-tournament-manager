4cb816acb1dd380739a552595ef4f31db621817b
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-10-09 03:07:01 +0200

    ci: migrate from vitest workspace to projects

--- vitest.workspace.ts
+++
@@ -1,6 +0,0 @@
-import { defineWorkspace } from 'vitest/config';
-
-export default defineWorkspace([
-  'apps/*/vitest.config.{integration,unit}.ts',
-  'packages/*/vitest.config.{integration,unit}.ts',
-]);
