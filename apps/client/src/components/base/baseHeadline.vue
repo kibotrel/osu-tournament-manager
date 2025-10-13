3ad853d0d3aa29f85041e2c9c7992181f03b6b1a
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-10-13 02:39:45 +0200

    feat(apps/client/components): add BaseHeadline

---
+++ apps/client/src/components/base/baseHeadline.vue
@@ -0,0 +1,36 @@
+<template>
+  <component
+    :is="properties.variant === 'subtitle' ? 'h2' : 'h1'"
+    :class="properties.variant"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script setup lang="ts">
+export type HeadlineVariant = 'hero' | 'subtitle' | 'title';
+
+interface Properties {
+  variant?: HeadlineVariant;
+}
+
+const properties = withDefaults(defineProps<Properties>(), {
+  variant: 'title',
+});
+</script>
+
+<style scoped>
+@reference '#src/assets/styles/index.css';
+
+.hero {
+  @apply text-4xl font-bold tracking-tighter;
+}
+
+.title {
+  @apply text-2xl font-semibold tracking-tight;
+}
+
+.subtitle {
+  @apply text-xl tracking-tight;
+}
+</style>
