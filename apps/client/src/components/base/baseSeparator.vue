e4ea58cb2d4aefc2f1a844450af5912a9151ef0c
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-12-13 15:51:16 +0100

    feat(apps/client/components): add BaseSeparator

---
+++ apps/client/src/components/base/baseSeparator.vue
@@ -0,0 +1,27 @@
+<template>
+  <hr :class="properties.variant" />
+</template>
+
+<script setup lang="ts">
+type SeparatorVariant = 'dashed' | 'primary';
+
+interface Properties {
+  variant?: SeparatorVariant;
+}
+
+const properties = withDefaults(defineProps<Properties>(), {
+  variant: 'primary',
+});
+</script>
+
+<style scoped>
+@reference '#src/assets/styles/index.css';
+
+hr {
+  @apply border-primary-3 border-b-1;
+}
+
+.dashed {
+  @apply border-dashed;
+}
+</style>
