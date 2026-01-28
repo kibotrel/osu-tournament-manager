7c2e53d72d55bc8f6d96cf2edede1c0d3decba91
BOTREL Kilian <botrel.kilian1999@gmail.com>
2026-01-28 23:45:34 +0100

    chore(apps/server/queries): sort method declaration in updateMatchQueries.ts

--- apps/server/src/queries/matches/updateMatchQueries.ts
+++ apps/server/src/queries/matches/updateMatchQueries.ts
@@ -4,18 +4,6 @@ import { database } from '#src/dependencies/databaseDependency.js';
 import type { InsertMatch } from '#src/schemas/matches/matchesTable.js';
 import { matchesTable } from '#src/schemas/matches/matchesTable.js';
 
-export const patchMatchById = async (
-  id: number,
-  options: Partial<Omit<InsertMatch, 'id' | 'createdAt' | 'updatedAt'>>,
-) => {
-  const { rowCount } = await database
-    .update(matchesTable)
-    .set(options)
-    .where(eq(matchesTable.id, id));
-
-  return { success: rowCount === 1 };
-};
-
 export const patchMatchByGameMatchId = async (
   gameMatchId: number,
   options: Partial<Omit<InsertMatch, 'id' | 'createdAt' | 'updatedAt'>>,
@@ -24,6 +12,18 @@ export const patchMatchByGameMatchId = async (
     .update(matchesTable)
     .set(options)
     .where(eq(matchesTable.gameMatchId, gameMatchId));
+
+  return { success: rowCount === 1 };
+};
+
+export const patchMatchById = async (
+  id: number,
+  options: Partial<Omit<InsertMatch, 'id' | 'createdAt' | 'updatedAt'>>,
+) => {
+  const { rowCount } = await database
+    .update(matchesTable)
+    .set(options)
+    .where(eq(matchesTable.id, id));
 
   return { success: rowCount === 1 };
 };
